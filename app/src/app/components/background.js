bfebd8bf0622afeaf65b6e67351e8e80d248fdf7
Hiatusk <33849198+Hiatusk@users.noreply.github.com>
2024-09-28 09:08:29 -0400

    Front-end first step.

---
+++ app/src/app/components/background.js
@@ -0,0 +1,16 @@
+import "./background.css"
+
+function Background() {
+    return (
+        <div id="bg" className="z-0 absolute min-h-screen min-w-full">
+            <div id="cir-1" className="circle"></div>
+            <div id="cir-2" className="circle"></div>
+            <div id="cir-3" className="circle"></div>
+            <div id="cir-4" className="circle"></div>
+            <div id="cir-5" className="circle"></div>
+            <div id="cir-6" className="circle"></div>
+        </div>
+    );
+}
+
+export default Background
--- app/src/app/login/page.js
+++ app/src/app/login/page.js
@@ -1,6 +1,7 @@
 "use client";
 
-import './style.css'
+import Background from "../components/background.js"
+import "./style.css"
 
 function Login() {
     const onSubmit = async (event) => {
@@ -21,34 +22,35 @@ function Login() {
     }
 
     return (
-      <div id="wrapper" className="flex flex-col items-center justify-center min-h-screen">
-        <form className="flex flex-col items-center gap-2 w-3/5" onSubmit={onSubmit}>
-            <div className='flex flex-col gap-2 w-4/5'>
-                <span className='text-2xl my-6 font-extrabold'>Sign in to your account</span>
-                <label>
-                    Username
-                    <input 
-                        className="w-full py-2 px-1 rounded"
-                        type="text" 
-                        title='username'
-                        name="username"
-                        placeholder="John Doe">
-                    </input>
-                </label>
-                <label>
-                    Password
-                    <input 
-                        className="w-full py-2 px-1 rounded"
-                        type="password" 
-                        title='password'
-                        name="password" 
-                        placeholder="***********">
-                        </input>
-                </label>
+        <>
+            <div id="wrapper" className="flex flex-col items-center justify-center min-h-screen">
+                <form className="flex flex-col items-center gap-2 w-3/5 z-10 py-10" onSubmit={onSubmit}>
+                    <div className='flex flex-col gap-4 w-4/5'>
+                        <span className='text-2xl mt-4 mb-8 font-black'>Sign in to your account</span>
+                        <label>
+                            Username
+                            <input 
+                                type="text" 
+                                title='username'
+                                name="username"
+                                placeholder="John Doe">
+                            </input>
+                        </label>
+                        <label>
+                            Password
+                            <input 
+                                type="password" 
+                                title='password'
+                                name="password" 
+                                placeholder="***********">
+                                </input>
+                        </label>
+                        <input className="cursor-pointer w-full mt-12 mb-3 py-1 rounded" type="submit"></input>
+                    </div>
+                </form>
+                <Background />
             </div>
-            <input className="w-full mt-20" type="submit"></input>
-        </form>
-      </div>
+        </>
     );
   }
 
