d7ade554d54f2fbbb1379b8e63e3d292fc06d01e
EdisonnTran <tran.edison52@gmail.com>
2024-09-28 09:03:10 -0400

    Add mission modal works(?)

--- app/src/app/home/Components/addMission.js
+++ app/src/app/home/Components/addMission.js
@@ -1,34 +1,24 @@
-export default function AddMission(v) {
+'use client'
+import React, { useState } from 'react';
+import Modal from './Modal.js'
+
+
+export default function AddMission() {
+
+   const [state, setState] = useState(false);
+
+   const openModal = () => {
+        setState(true);
+   }
+
+   const closeModal = () => setState(false);
 
     return (
-        <div className="m-5">
-            <h1>Add a Mission!</h1>
-            <form action="/" className="flex flex-col gap-1">
-                <input
-                    className="bg-white text-gray-500"
-                    id="title"
-                    name="title"
-                    placeholder="Title"
-                />
-                <input
-                    className="bg-white text-gray-500"
-                    id="description"
-                    name="description"
-                    placeholder="Description"
-                />
-                <input
-                    className="bg-white text-gray-500"
-                    id="location"
-                    name="location"
-                    placeholder="Location"
-                />
-                <input
-                    className="bg-gray-500 text-white"
-                    type="submit"
-                
-                />
-            </form>
-            
+
+        <div >
+            <button className="bg-gray-500 font-bold" onClick={openModal}>Start Mission {String(state)} </button>
+            <Modal show={state} onClose={closeModal} />
         </div>
+
     )
 }
--- app/src/app/home/page.js
+++ app/src/app/home/page.js
@@ -3,29 +3,29 @@ import EditMission from './Components/editMission.js';
 
 export default function Home() {
     return (
-<<<<<<< HEAD
-      <div className="flex flex-col center-items ml-64 mr-64 min-h-screen">
-        <h1>let's get some work done (woo)</h1>
-
-        <h2>Current Mission:</h2>
-
-        <div className="border-red-500 border-4 w-full h-40">display something!</div>
-
-          <div>
-            {/* Display all missions */}
+      <div className="min-h-screen">
+        <div className="flex justify-between ml-64 mr-64 pt-8 h-full">
+          <h1>let's get some work done (woo)</h1>
+            <div>
+              {/* Add a missison */}
+                <AddMission></AddMission>
+              </div>
           </div>
-          <div>
-            {/* Add a missison */}
-            <AddMission></AddMission>
+
+          <div className="flex flex-col center-items ml-64 mr-64 mt-16">
+            <h2>Current Mission:</h2>
+
+            <div className=" border-red-500 border-4 w-full h-40">display something!</div>
+
+              <div>
+                {/* Display all missions */}
+              </div>
+              <div>
+                {/* Cancel a mission */}
+                <EditMission></EditMission>
+              </div>
           </div>
-          <div>
-            {/* Cancel a mission */}
-            <EditMission></EditMission>
-          </div>
-=======
-      <div className="flex flex-row center-items">
-        <h1>Hello Test!</h1>
->>>>>>> 6ef1b8f8df38201f3c560b398111de544e6de7bd
       </div>
+
     );
   }
