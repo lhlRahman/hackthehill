dbad975a575d7d113e6a8073aa7ab5a2c974164b
Hiatusk <33849198+Hiatusk@users.noreply.github.com>
2024-09-28 10:06:52 -0400

    Backend bridge intermediary.

--- app/src/app/login/page.js
+++ app/src/app/login/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import Background from "../components/background.js"
 import "./style.css"
 
@@ -11,43 +12,44 @@ function Login() {
         const username = data.get('username')
         const password = data.get('password')
 
-        const response = await fetch('/api/auth/', {
+        const response = await fetch('http://localhost:3001/user/login/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({username, password})
         })
-        const content = await response.text();
     }
 
     return (
         <>
-            <div id="wrapper" className="flex flex-col items-center justify-center min-h-screen">
-                <form className="flex flex-col items-center gap-2 w-3/5 z-10 py-10" onSubmit={onSubmit}>
-                    <div className='flex flex-col gap-4 w-4/5'>
-                        <span className='text-2xl mt-4 mb-8 font-black'>Sign in to your account</span>
-                        <label>
-                            Username
-                            <input 
-                                type="text" 
-                                title='username'
-                                name="username"
-                                placeholder="John Doe">
-                            </input>
-                        </label>
-                        <label>
-                            Password
-                            <input 
-                                type="password" 
-                                title='password'
-                                name="password" 
-                                placeholder="***********">
+            <div id="wrapper" className="flex flex-col items-center justify-center h-screen">
+                <Suspense fallback={<p>Loading...</p>}>
+                    <form className="flex flex-col items-center gap-4 w-3/5 z-10 py-10" onSubmit={onSubmit}>
+                        <div className='flex flex-col gap-4 w-4/5'>
+                            <span className='text-3xl mt-4 mb-10 font-black'>Sign in to your account</span>
+                            <label>
+                                Username
+                                <input 
+                                    type="text" 
+                                    title='username'
+                                    name="username"
+                                    placeholder="John Doe">
                                 </input>
-                        </label>
-                        <input className="cursor-pointer w-full mt-12 mb-3 py-1 rounded" type="submit"></input>
-                    </div>
-                </form>
+                            </label>
+                            <label>
+                                Password
+                                <input 
+                                    type="password" 
+                                    title='password'
+                                    name="password" 
+                                    placeholder="***********">
+                                    </input>
+                            </label>
+                            <input className="cursor-pointer w-full mt-14 mb-3 py-1 rounded" type="submit"></input>
+                        </div>
+                    </form>
+                </Suspense>
                 <Background />
             </div>
         </>
