cf08b0f7ad233a015dcf392128a308e9f6c3f10d
EdisonnTran <tran.edison52@gmail.com>
2024-09-28 11:36:34 -0400

    modal test

--- app/src/app/home/Components/addMission.js
+++ app/src/app/home/Components/addMission.js
@@ -5,6 +5,7 @@ import Modal from './Modal.js'
 
 export default function AddMission() {
 
+
    const [state, setState] = useState(false);
 
    const openModal = () => {
@@ -13,10 +14,14 @@ export default function AddMission() {
 
    const closeModal = () => setState(false);
 
+    // closeModal();
+
+
     return (
 
         <div >
-            <button className="bg-gray-500 font-bold" onClick={openModal}>Start Mission {String(state)} </button>
+            <button className="bg-gray-500 font-bold" onClick={openModal}>Start Mission</button>
+            
             <Modal show={state} onClose={closeModal} />
         </div>
 
