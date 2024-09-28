beab5924767597be070bc1dc163e122c5a83fcc2
Abdul <lhlrahman@gmail.com>
2024-09-28 10:37:09 -0400

    Refactor login page component and update backend configuration

--- app/src/app/login/page.js
+++ app/src/app/login/page.js
@@ -1,57 +1,82 @@
 "use client";
 
-import { Suspense } from "react";
-import "./style.css"
+import { useState } from "react";
+import "./style.css";
 
-function Login() {
-    const onSubmit = async (event) => {
-        event.preventDefault()
+export default function Login() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [reveal, setReveal] = useState(false);
 
-        const data = new FormData(event.target)
-        const username = data.get('username')
-        const password = data.get('password')
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
 
-        const response = await fetch('http://localhost:3001/user/login/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({username, password})
-        })
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      window.location.replace("/");
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
     }
+  };
 
-    return (
-        <>
-            <div id="wrapper" className="flex flex-col items-center justify-center h-screen">
-                <Suspense fallback={<p>Loading...</p>}>
-                    <form className="flex flex-col items-center gap-4 w-3/5 z-10 py-10" onSubmit={onSubmit}>
-                        <div className='flex flex-col gap-4 w-4/5'>
-                            <span className='text-3xl mt-4 mb-10 font-black'>Sign in to your account</span>
-                            <label>
-                                Username
-                                <input 
-                                    type="text" 
-                                    title='username'
-                                    name="username"
-                                    placeholder="John Doe">
-                                </input>
-                            </label>
-                            <label>
-                                Password
-                                <input 
-                                    type="password" 
-                                    title='password'
-                                    name="password" 
-                                    placeholder="***********">
-                                    </input>
-                            </label>
-                            <input className="cursor-pointer w-full mt-14 mb-3 py-1 rounded" type="submit"></input>
-                        </div>
-                    </form>
-                </Suspense>
-            </div>
-        </>
-    );
-  }
-
-export default Login
+  return (
+    <div id="wrapper" className="flex flex-col items-center justify-center min-h-screen">
+      <form className="flex flex-col items-center gap-2 w-3/5" onSubmit={onSubmit}>
+        <div className='flex flex-col gap-2 w-4/5'>
+          <span className='text-2xl my-6 font-extrabold'>Sign in to your account</span>
+          <label>
+            Username
+            <input 
+              className="w-full py-2 px-1 rounded"
+              type="text" 
+              title='username'
+              name="username"
+              value={inputs.username}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              className="w-full py-2 px-1 rounded"
+              type={reveal ? "text" : "password"}
+              title='password'
+              name="password"
+              value={inputs.password}
+              onChange={onChange}
+            />
+          </label>
+          <div
+            className={`${reveal ? "reveal" : "lock"}`}
+            onClick={() => {
+              setReveal(!reveal);
+            }}
+          >
+            <img src="/icons/lockIcon.svg" alt="lock icon" />
+            <img src="/icons/eyeIcon.svg" alt="open icon" />
+          </div>
+          <button type="submit" className="mt-4 py-2 px-4 bg-blue-500 text-white rounded">Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
+}
