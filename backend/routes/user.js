11605b20f3594e25b0e862b08f15ebccc9fe9e17
Abdul <lhlrahman@gmail.com>
2024-09-28 21:54:46 -0400

    Refactor user controller and routes: fix formatting in userRouter.get("/balance")

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 
 userRouter.post("/register", userControllers.registerUser);
 userRouter.post("/login", userControllers.loginUser);
-userRouter.get("/balance",userControllers.getBalance);
+userRouter.get("/balance", userControllers.getBalance);
 userRouter.get("/friends", userControllers.getFriends);
 userRouter.post("/add-friend", userControllers.addFriend);
 userRouter.put("/updatepoints", userControllers.updateBalance);
